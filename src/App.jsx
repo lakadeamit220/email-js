a5792d7e5f7392d362396cd0cb3b4945cd484744
Amit Lakade <99075733+lakadeamit220@users.noreply.github.com>
2025-06-05 17:50:33 +0530

    Implemented Contact Us using Email JS.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
+import { ContactUs } from "./components/Demo";
+
 function App() {
   return (
     <>
       <h1 className="text-center text-5xl text-red-500">Email JS</h1>
+      <ContactUs />
     </>
   );
 }
